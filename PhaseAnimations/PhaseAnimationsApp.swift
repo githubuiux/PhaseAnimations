92ce8ae952a875a4fac20fd96caca5586f2839ef
githubuiux <uiuxista@gmail.com>
2023-06-15 14:39:37 +0300

    Initial Commit

---
+++ PhaseAnimations/PhaseAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhaseAnimationsApp.swift
+//  PhaseAnimations
+//
+//  Created by Levent on 15.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhaseAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
